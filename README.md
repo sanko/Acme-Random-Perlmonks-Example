fa9deb92ebfc37c03b0a4ec338a071bc8368d851
Sanko Robinson <sanko@cpan.org>
2023-11-28 15:29:35 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Acme::Random::Perlmonks::Example - It's new $module
+
+# SYNOPSIS
+
+    use Acme::Random::Perlmonks::Example;
+
+# DESCRIPTION
+
+Acme::Random::Perlmonks::Example is ...
+
+# LICENSE
+
+Copyright (C) Sanko Robinson.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Sanko Robinson <sanko@cpan.org>
---
+++ lib/Acme/Random/Perlmonks/Example.pm
@@ -0,0 +1,39 @@
+package Acme::Random::Perlmonks::Example;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Random::Perlmonks::Example - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::Random::Perlmonks::Example;
+
+=head1 DESCRIPTION
+
+Acme::Random::Perlmonks::Example is ...
+
+=head1 LICENSE
+
+Copyright (C) Sanko Robinson.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Sanko Robinson E<lt>sanko@cpan.orgE<gt>
+
+=cut
+
